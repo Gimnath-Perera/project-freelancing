192a1abf9a6cd8542cbb7d27321d35ac95f42a6c
Gimnath-Perera <gimnathperera@gmail.com>
2020-02-04 09:51:52 +0530

    animations in home-page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useWindowScrollPosition from '@rehooks/window-scroll-position';
+
 import './App.css';
 
 const App = () => {
+  const [navChange, setnavChange] = useState(false);
+
+  const changePosition = 200;
+  let position = useWindowScrollPosition();
+  if (position.y > changePosition && !navChange) {
+    setnavChange(true);
+  }
+  if (position.y <= changePosition && navChange) {
+    setnavChange(false);
+  }
+
+  let style = {
+    backgroundColor: navChange ? `#2C3E50` : ``,
+    paddingTop: navChange ? 0 : ``,
+    paddingBottom: navChange ? 0 : ``
+  };
+
   return (
     <div>
       <nav
-        className='navbar navbar-expand-lg bg-secondary text-uppercase fixed-top'
+        className='navbar navbar-expand-lg  text-uppercase fixed-top'
         id='mainNav'
+        style={style}
       >
         <div className='container'>
-          <a className='navbar-brand js-scroll-trigger' href='#page-top'>
+          <a
+            className='navbar-brand js-scroll-trigger  cssanimation leFadeInTop sequence hvr-buzz-out'
+            href='#page-top'
+          >
             Start Bootstrap
           </a>
           <button
@@ -28,7 +51,7 @@ const App = () => {
             <ul className='navbar-nav ml-auto'>
               <li className='nav-item mx-0 mx-lg-1'>
                 <a
-                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger'
+                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger hvr-buzz-out cssanimation leFadeInTop sequence'
                   href='#portfolio'
                 >
                   Portfolio
@@ -36,7 +59,7 @@ const App = () => {
               </li>
               <li className='nav-item mx-0 mx-lg-1'>
                 <a
-                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger'
+                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger hvr-buzz-out cssanimation leFadeInTop sequence'
                   href='#about'
                 >
                   About
@@ -44,7 +67,7 @@ const App = () => {
               </li>
               <li className='nav-item mx-0 mx-lg-1'>
                 <a
-                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger'
+                  className='nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger hvr-buzz-out cssanimation leFadeInTop sequence'
                   href='#contact'
                 >
                   Contact
@@ -55,11 +78,20 @@ const App = () => {
         </div>
       </nav>
 
-      <header className='masthead bg-primary text-white text-center' id='page-top'>
+      <header
+        className='masthead bg-primary text-white text-center'
+        id='page-top'
+      >
         <div className='container d-flex align-items-center flex-column'>
-          <img className='masthead-avatar mb-5' src='https://picsum.photos/200/300' alt='' />
+          <img
+            className='masthead-avatar mb-5'
+            src='https://picsum.photos/200/300'
+            alt=''
+          />
 
-          <h1 className='masthead-heading text-uppercase mb-0'>Start Bootstrap</h1>
+          <h1 className='masthead-heading text-uppercase mb-0'>
+            Start Bootstrap
+          </h1>
 
           <div className='divider-custom divider-light'>
             <div className='divider-custom-line'></div>
@@ -77,7 +109,7 @@ const App = () => {
 
       <section className='page-section portfolio' id='portfolio'>
         <div className='container'>
-          <h2 className='page-section-heading text-center text-uppercase text-secondary mb-0'>
+          <h2 className='page-section-heading text-center text-uppercase text-secondary mb-0 cssanimation hu__hu__ sequence'>
             Portfolio
           </h2>
 
@@ -101,7 +133,11 @@ const App = () => {
                     <i className='fa fa-plus fa-3x'></i>
                   </div>
                 </div>
-                <img className='img-fluid' src='https://picsum.photos/200/300' alt='' />
+                <img
+                  className='img-fluid'
+                  src='https://picsum.photos/200/300'
+                  alt=''
+                />
               </div>
             </div>
 
@@ -116,7 +152,11 @@ const App = () => {
                     <i className='fa fa-plus fa-3x'></i>
                   </div>
                 </div>
-                <img className='img-fluid' src='https://picsum.photos/200/300' alt='' />
+                <img
+                  className='img-fluid'
+                  src='https://picsum.photos/200/300'
+                  alt=''
+                />
               </div>
             </div>
 
@@ -131,7 +171,11 @@ const App = () => {
                     <i className='fa fa-plus fa-3x'></i>
                   </div>
                 </div>
-                <img className='img-fluid' src='https://picsum.photos/200/300' alt='' />
+                <img
+                  className='img-fluid'
+                  src='https://picsum.photos/200/300'
+                  alt=''
+                />
               </div>
             </div>
 
@@ -146,7 +190,11 @@ const App = () => {
                     <i className='fa fa-plus fa-3x'></i>
                   </div>
                 </div>
-                <img className='img-fluid' src='https://picsum.photos/200/300' alt='' />
+                <img
+                  className='img-fluid'
+                  src='https://picsum.photos/200/300'
+                  alt=''
+                />
               </div>
             </div>
 
@@ -161,7 +209,11 @@ const App = () => {
                     <i className='fa fa-plus fa-3x'></i>
                   </div>
                 </div>
-                <img className='img-fluid' src='https://picsum.photos/200/300' alt='' />
+                <img
+                  className='img-fluid'
+                  src='https://picsum.photos/200/300'
+                  alt=''
+                />
               </div>
             </div>
 
@@ -187,9 +239,9 @@ const App = () => {
         </div>
       </section>
 
-      <section className='page-section bg-primary text-white mb-0' id='about'>
+      <section className='page-section bg-primary text-white mb-0 ' id='about'>
         <div className='container'>
-          <h2 className='page-section-heading text-center text-uppercase text-white'>
+          <h2 className='page-section-heading text-center text-uppercase text-white cssanimation leSnake sequence'>
             About
           </h2>
 
@@ -202,16 +254,16 @@ const App = () => {
           </div>
 
           <div className='row'>
-            <div className='col-lg-4 ml-auto'>
-              <p className='lead'>
+            <div className='col-lg-4 ml-auto '>
+              <p className='lead hvr-pulse'>
                 Freelancer is a free bootstrap theme created by Start Bootstrap.
                 The download includes the complete source files including HTML,
                 CSS, and JavaScript as well as optional SASS stylesheets for
                 easy customization.
               </p>
             </div>
-            <div className='col-lg-4 mr-auto'>
-              <p className='lead'>
+            <div className='col-lg-4 mr-auto '>
+              <p className='lead hvr-pulse'>
                 You can create your own custom avatar for the masthead, change
                 the icon in the dividers, and add your email address to the
                 contact form to make it fully functional!
@@ -220,10 +272,7 @@ const App = () => {
           </div>
 
           <div className='text-center mt-4'>
-            <a
-              className='btn btn-xl btn-outline-light'
-              href=''
-            >
+            <a className='btn btn-xl btn-outline-light hvr-buzz-out' href=''>
               <i className='fa fa-download mr-2'></i>
               Free Download!
             </a>
@@ -233,7 +282,7 @@ const App = () => {
 
       <section className='page-section' id='contact'>
         <div className='container'>
-          <h2 className='page-section-heading text-center text-uppercase text-secondary mb-0'>
+          <h2 className='page-section-heading text-center text-uppercase text-secondary mb-0 cssanimation leWaterWave sequence'>
             Contact Us
           </h2>
 
@@ -327,9 +376,9 @@ const App = () => {
             <div className='col-lg-4 mb-5 mb-lg-0'>
               <h4 className='text-uppercase mb-4'>Location</h4>
               <p className='lead mb-0'>
-               No 388/5 Dekatana road
+                No 388/5 Dekatana road
                 <br />
-               Nagas junction, Mandawala
+                Nagas junction, Mandawala
               </p>
             </div>
 
@@ -411,11 +460,11 @@ const App = () => {
                       <div className='divider-custom-line'></div>
                     </div>
                     <img
-                      className='img-fluid rounded mb-5'
+                      className='img-fluid rounded mb-5 '
                       src='https://picsum.photos/200/300'
                       alt=''
                     />
-                    <p className='mb-5'>
+                    <p className='mb-5 '>
                       Lorem ipsum dolor sit amet, consectetur adipisicing elit.
                       Mollitia neque assumenda ipsam nihil, molestias magnam,
                       recusandae quos quis inventore quisquam velit asperiores,
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './assets/css/freelancer.min.css';
 import './index.css';
+import './assets/css/bootstrap.min.css';
+import './assets/css/hover-min.css';
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
